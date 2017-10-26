3c447324fc813c04af39f1586caa877a6ba31e2a
Guillaume Huysmans <ghuysmans99@gmail.com>
2017-10-26 08:47:13 +0200

    Added colors to show more information
    We should disable highlighting when the output isn't a terminal, though.

--- menu.ml
+++ menu.ml
@@ -1,5 +1,34 @@
 open Lwt.Infix
 
+let has_pork = List.mem "CONTIENT DU PORC"
+let is_cold = List.mem "SE MANGE FROID"
+let is_veggie = List.mem "VÉGÉTARIEN"
+let is_vegan = List.mem "VEGAN"
+
+let print_prod {Foodcheri.product_type; picture; chef; title; tags; price; _} =
+  let pt = String.sub product_type 0 2 in
+  let open ANSITerminal in
+  let fg =
+    if has_pork tags then
+      magenta
+    else if is_veggie tags then
+      green
+    else if is_vegan tags then
+      blue
+    else
+      default
+  in
+  let st =
+    fg ::
+    if is_cold tags then
+      []
+    else
+      [Bold]
+  in
+  (* FIXME test whether stdout is a terminal *)
+  printf st "%s | %s\n" pt title;
+  Lwt.return_unit
+
 (* TODO cache *)
 let () = Lwt_main.run (
   (*
@@ -7,14 +36,6 @@ let () = Lwt_main.run (
   let%lwt html = get_menu_html shift in
   FIXME stream using from_signals?
   *)
-  let fmt {Foodcheri.product_type; picture; chef; title; tags; price; _} =
-    let pt = String.sub product_type 0 2 in
-    (*
-    Printf.sprintf "%s | %5.2f€ | %s\n" pt price title
-    *)
-    Printf.sprintf "%s | %s\n" pt title
-  in
   Foodcheri.get_menu {Foodcheri.idShift=42; deliveryDate="x"}
-  >|= List.map fmt
-  >>= Lwt_list.iter_s Lwt_io.print
+  >>= Lwt_list.iter_s print_prod
 )
