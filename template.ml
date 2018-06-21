3a668dfa1224f6170a94fded837f57223a012c74
Guillaume Huysmans <ghuysmans99@gmail.com>
2018-06-22 00:36:37 +0200

    Unsafe CSS

--- template.ml
+++ template.ml
@@ -5,6 +5,19 @@ let page page_title content =
   let open Tyxml.Html in
   let title = title (pcdata page_title) in
   let h1 = h1 [pcdata page_title] in
-  let css = "label {display: block}" in
+  let css = {|
+label {display: block}
+button[type="reset"] {float: right}
+table {background-color: lightgrey}
+table td:first-child:not([colspan]) {text-align: right}
+table.synth td:first-child, table.synth td:nth-child(2) {
+  width: 20%;
+  text-align: left;
+}
+table.synth_c td:first-child, table.synth_c td:nth-child(2) {
+  width: 20%;
+  text-align: center;
+}
+  |} in
   let utf8 = meta ~a:[a_charset "utf8"] () in
-  html (head title [utf8; style [pcdata css]]) (body (h1 :: content))
+  html (head title [utf8; style [Unsafe.data css]]) (body (h1 :: content))
